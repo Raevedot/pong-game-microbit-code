4bc2b44e76544e23d66b0321cf4bf1cef13bb4d5
Raevedot <150303132+Raevedot@users.noreply.github.com>
2023-11-09 00:07:06 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,104 +1,132 @@
 input.onButtonPressed(Button.A, function () {
-    if (led.point(0, 4)) {
-        led.plot(0, 3)
-        led.unplot(0, 4)
-    } else if (led.point(0, 3)) {
-        led.plot(0, 2)
-        led.unplot(0, 3)
-    } else if (led.point(0, 2)) {
-        led.plot(0, 1)
-        led.unplot(0, 2)
-    } else if (led.point(0, 1)) {
-        led.plot(0, 0)
-        led.unplot(0, 1)
-    } else if (led.point(0, 0)) {
-        led.plot(0, 4)
-        led.unplot(0, 0)
-    }
+    playera = playera % 5 + 1
+    led.plot(0, playera - 1)
+    led.unplot(0, playerapos[playera])
 })
 input.onButtonPressed(Button.B, function () {
-    if (led.point(4, 0)) {
-        led.plot(4, 1)
-        led.unplot(4, 0)
-    } else if (led.point(4, 1)) {
-        led.plot(4, 2)
-        led.unplot(4, 1)
-    } else if (led.point(4, 2)) {
-        led.plot(4, 3)
-        led.unplot(4, 2)
-    } else if (led.point(4, 3)) {
-        led.plot(4, 4)
-        led.unplot(4, 3)
-    } else if (led.point(4, 4)) {
-        led.plot(4, 0)
-        led.unplot(4, 4)
-    }
+    playerb = playerb % 5 + 1
+    led.plot(4, Math.abs(playerb - 5))
+    led.unplot(4, playerbpos[playerb])
 })
-let bally2 = 0
-let ballx2 = 0
-led.plot(0, 4)
-led.plot(4, 0)
-let ball = 7
-let ballx1 = 2
-let bally1 = 2
-let ballpos = [
+input.onGesture(Gesture.Shake, function () {
+    led.plot(4, 4)
+    led.plot(0, 0)
+    playera = 1
+    playerb = 1
+    ballx = 2
+    bally = 2
+    rng1 = randint(0, 3)
+})
+let rng2 = 0
+let rng1 = 0
+let bally = 0
+let ballx = 0
+let playerbpos: number[] = []
+let playerapos: number[] = []
+let playerb = 0
+let playera = 0
+led.plot(4, 4)
+led.plot(0, 0)
+playera = 1
+playerb = 1
+playerapos = [
+0,
+4,
 0,
 1,
 2,
-3,
-4,
-5,
-6,
-7,
-8,
-9,
-10,
-11,
-12,
-13,
-14
+3
 ]
+playerbpos = [
+0,
+0,
+4,
+3,
+2,
+1
+]
+ballx = 2
+bally = 2
+rng1 = randint(0, 3)
 basic.forever(function () {
-    if (ball == ballpos[0]) {
-        led.plot(1, 0)
-        led.unplot(ballx1, bally1)
-        ballx1 = 1
-        bally1 = 0
-    } else if (ball == ballpos[1]) {
-        led.plot(2, 0)
-    } else if (ball == ballpos[2]) {
-        led.plot(3, 0)
-    } else if (ball == ballpos[3]) {
-        led.plot(1, 1)
-    } else if (ball == ballpos[4]) {
-        led.plot(2, 1)
-    } else if (ball == ballpos[5]) {
-        led.plot(3, 1)
-    } else if (ball == ballpos[6]) {
-        led.plot(1, 2)
-    } else if (ball == ballpos[7]) {
-        led.plot(2, 2)
-        ballx2 = 2
-        bally2 = 2
-        led.unplot(ballx1, bally1)
-        ballx1 = 2
-        bally1 = 2
-    } else if (ball == ballpos[8]) {
-        led.plot(3, 2)
-    } else if (ball == ballpos[9]) {
-        led.plot(1, 3)
-    } else if (ball == ballpos[10]) {
-        led.plot(2, 3)
-    } else if (ball == ballpos[11]) {
-        led.plot(3, 3)
-    } else if (ball == ballpos[12]) {
-        led.plot(1, 4)
-    } else if (ball == ballpos[13]) {
-        led.plot(2, 4)
-    } else if (ball == ballpos[14]) {
-        led.plot(3, 4)
+    basic.pause(1000)
+    if (rng1 == 0) {
+        while (ballx != 5) {
+            ballx = ballx + 1
+            bally = bally - 1
+            led.unplot(ballx - 1, bally + 1)
+            basic.pause(1000)
+        }
+    } else if (rng1 == 1) {
+        while (bally != -1) {
+            ballx = ballx - 1
+            bally = bally + 1
+            led.unplot(ballx + 1, bally - 1)
+            basic.pause(1000)
+        }
+    } else if (rng1 == 2) {
+        while (bally != -1) {
+            ballx = ballx - 1
+            bally = bally - 1
+            led.unplot(ballx + 1, bally + 1)
+            basic.pause(1000)
+        }
+    } else if (rng1 == 3) {
+        while (ballx != 5) {
+            ballx = ballx + 1
+            bally = bally + 1
+            led.unplot(ballx - 1, bally - 1)
+            basic.pause(1000)
+        }
     }
 })
 basic.forever(function () {
-	
+    if (ballx == 5) {
+        basic.showString("Player A wins!")
+    } else if (ballx == -1) {
+        basic.showString("Player B wins!")
+    }
+})
+basic.forever(function () {
+    led.plot(ballx, bally)
+})
+basic.forever(function () {
+    if (led.point(ballx + 1, bally)) {
+        rng2 = randint(0, 1)
+        if (rng2 == 0) {
+            ballx = ballx - 1
+            bally = bally + 1
+            led.unplot(ballx + 1, bally - 1)
+            basic.pause(1000)
+        } else if (rng2 == 1) {
+            ballx = ballx - 1
+            bally = bally - 1
+            led.unplot(ballx + 1, bally - 1)
+            basic.pause(1000)
+        }
+    }
+    if (led.point(ballx - 1, bally)) {
+        rng2 = randint(0, 1)
+        if (rng2 == 0) {
+            ballx = ballx + 1
+            bally = bally - 1
+            led.unplot(ballx + 1, bally - 1)
+            basic.pause(1000)
+        } else if (rng2 == 1) {
+            ballx = ballx - 1
+            bally = bally - 1
+            led.unplot(ballx + 1, bally - 1)
+            basic.pause(1000)
+        }
+    }
+})
+basic.forever(function () {
+    if (bally == 4) {
+        basic.pause(1000)
+        bally = bally - 1
+        rng1 = 0
+    } else if (bally == 0) {
+        basic.pause(1000)
+        bally = bally + 1
+    }
 })
